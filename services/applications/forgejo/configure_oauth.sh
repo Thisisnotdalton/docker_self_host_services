6049a437509965c6f2ce9331d155f8c4c218166a
Dalton <dalton@pukiru.games>
2026-02-27 15:58:54 -0500

    Deduplicate arguments for oauth update/creation.

--- services/applications/forgejo/configure_oauth.sh
+++ services/applications/forgejo/configure_oauth.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # Capture auth list output
@@ -13,10 +13,17 @@ AUTH_ID="$(printf "%s\n" "$AUTH_LIST" | awk -v name="$OAUTH2_NAME" '
   }
 ')"
 
+args=(
+  --name "$OAUTH2_NAME"
+  --provider "$OAUTH2_PROVIDER"
+  --key "$OAUTH2_CLIENT_ID"
+  --secret "$CLIENT_SECRET"
+  --auto-discover-url "$OAUTH2_OPENID_CONNECT_AUTO_DISCOVERY_URL"
+  --scopes "openid email profile groups"
+)
+
 if [ -n "$AUTH_ID" ]; then
-  echo "Found auth source '$OAUTH2_NAME' with ID: $AUTH_ID"
-  forgejo admin auth update-oauth --id "${AUTH_ID}" --name "${OAUTH2_NAME}" --provider "${OAUTH2_PROVIDER}" --key "${OAUTH2_CLIENT_ID}" --secret "${CLIENT_SECRET}" --auto-discover-url "${OAUTH2_OPENID_CONNECT_AUTO_DISCOVERY_URL}" --scopes="openid email profile groups"
+  forgejo admin auth update-oauth --id "$AUTH_ID" "${args[@]}"
 else
-  echo "Auth source '$OAUTH2_NAME' not found."
-  forgejo admin auth add-oauth --name "${OAUTH2_NAME}" --provider "${OAUTH2_PROVIDER}" --key "${OAUTH2_CLIENT_ID}" --secret "${CLIENT_SECRET}" --auto-discover-url "${OAUTH2_OPENID_CONNECT_AUTO_DISCOVERY_URL}" --scopes="openid email profile groups"
+  forgejo admin auth add-oauth "${args[@]}"
 fi
